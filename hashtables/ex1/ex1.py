bc63bde3fa05bb5abc48c90c8c8b6b9205c63b5e
Samir Lilienfeld <samirlilienfeld@hotmail.com>
2020-02-14 11:54:21 -0500

    almost done with ex1

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -9,11 +9,17 @@ from hashtables import (HashTable,
 def get_indices_of_item_weights(weights, length, limit):
     ht = HashTable(16)
 
-    """
-    YOUR CODE HERE
-    """
-
-    return None
+    results_array = []
+    for i in range(length):
+        hash_table_insert(ht, weights[i], i)
+    for i in range(length):
+        target = limit - weights[i]
+        result = (hash_table_retrieve(ht, target)) 
+        results_array.append(result) 
+        clean_results = [i for i in results_array if i is not None]
+        if len(clean_results) == 0:
+            return None
+    return sorted(clean_results, reverse=True)
 
 
 def print_answer(answer):
@@ -21,3 +27,7 @@ def print_answer(answer):
         print(str(answer[0] + " " + answer[1]))
     else:
         print("None")
+
+
+
+#print(get_indices_of_item_weights([6,5,8,7,1], 5, 11))
